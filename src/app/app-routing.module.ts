c0c7cd146cdfbd4058bb4e96b802b0c292e5ec26
Gabriela Lima <lucaslopesllima@gmail.com>
2023-05-19 12:07:19 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { NotfoundComponent } from './components/notfound/notfound.component';
 const routes: Routes = [
  {path:'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
  {path:"", redirectTo:"produtos", pathMatch:"full"},
+ {path:"produtos", redirectTo:"produtos", pathMatch:"full"},
  {path:'carrinho', loadChildren: () => import('./carrinho/carrinho.module').then(m => m.CarrinhoModule) },
  {path:'contato', loadChildren: () => import('./contato/contato.module').then(m => m.ContatoModule) },
  {path:"**", component: NotfoundComponent} 
